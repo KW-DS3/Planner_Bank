cba3a584a2f16276fcb8af787444c3a7f917562c
jsi4603 <tjddls4603@gmail.com>
2020-12-15 15:27:29 +0900

    hash_map to unordered_map

--- keyword.hpp
+++ keyword.hpp
@@ -2,25 +2,26 @@
 #define __KEYWORD_H__
 
 #include <iostream>
-#include <vector>
 #include <map>
-#include <hash_map>
 #include <set>
+#include <unordered_map>
+#include <vector>
 
 using namespace std;
 
 class Keyword {
-    public:
-        void setkw(string title, string keyword);
-        void findkw(string title, string keyword);
-        void modifykw(string title, string keyword);
-        void deletekw(string title, string keyword);
-        void printkw(string title, string keyword);
-    private:
-        string title;
-        string kw;  //keyword
-        map<string, vector<string>> kwMap;
-        map<string, vector<string>>::iterator iter;
+  public:
+    void setkw(string title, string keyword);
+    void findkw(string title, string keyword);
+    void modifykw(string title, string keyword);
+    void deletekw(string title, string keyword);
+    void printkw(string title, string keyword);
+
+  private:
+    string title;
+    string kw; // keyword
+    map<string, vector<string>> kwMap;
+    map<string, vector<string>>::iterator iter;
 };
 
 #endif //__KEYWORD_H__
